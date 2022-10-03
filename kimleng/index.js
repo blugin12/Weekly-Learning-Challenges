b89699aff35e348fdefa24a710453a43c3c84308
blugin12 <112673527+blugin12@users.noreply.github.com>
2022-10-03 18:09:24 +0700

    Update index.js
    learn TypeScript in y minutes

--- kimleng/index.js
+++ kimleng/index.js
@@ -2547,3 +2547,206 @@
 //     .then(json => console.log(json))
 // }
 // work();
+
+// let isDone: boolean = false;
+// let lines: number = 42;
+// let name: string = "Anders";
+// console.log(isDone,lines,name);
+
+// let isDone = false;
+// let lines = 42;
+// let name = "Anders";
+// console.log(isDone, lines, name);
+
+// let notSure: any = 4;
+// notSure = "maybe a string instead";
+// notSure = false;
+// console.log(notSure);
+
+// code Error
+// const numLivesForCat = 9;
+// numLivesForCat = 1;
+// console.log(numLivesForCat);
+
+// let list: number[] = [1, 2, 3];
+// console.log(list);
+
+// let list: Array<number> = [1, 2, 3];
+// console.log(list);
+
+// let list: Array<any> = [true ,14, "sam"];
+// console.log(list);
+
+// enum Color { Red, Green, Blue};
+// let c: Color = Color.Green;
+// console.log(Color[c]);
+
+// function bigHorribleAlert(): void {
+//     console.log("I'm a little annoying box!");
+// }
+// bigHorribleAlert();
+
+// let f1 = function (i: number): number { return i * i; }
+// let f2 = function (i: number) {return i * i; }
+// let f3 = (i: number): number => { return i * i; }
+// let f4 = (i: number) => { return i * i; }
+// let f5 = (i: number) => i * i;
+// console.log(f1, f2, f3, f4, f5);
+
+// interface Person {
+//     name: string;
+//     age?: number;
+//     move(): void;
+// }
+// let p: Person = { name: "Bobby", move: () => { } };
+// let validPerson: Person = { name: "Bobby", age: 42, move: () => { } };
+// let invalidPerson: Person = { name: "Bobby", age: true };
+// console.log(p, validPerson, invalidPerson);
+
+// interface SearchFunc {
+//     (source: string, subString: string): boolean;
+// }
+// let mySearch: SearchFunc;
+// mySearch = function (src: string, sub: string) {
+//     return src.search(sub) != 1;
+// }
+// console.log(mySearch("nana","sam"));
+
+// class Point {
+//     x: number;
+//     constructor(x: number, public y: number = 0 ) {
+//         this.x = x;
+//     }
+//     dist(): number { return Math.sqrt(this.x * this.x + this .y * this.y); }
+//     static origin = new Point(0, 0);
+// }
+// class PointPerson implements Person {
+//     name: string
+//     move() {}
+// }
+// let p1 = new Point(10, 20);
+// let p2 = new Point(25);
+// class Point3D extends Point {
+//     constructor (x: number, y: number, public z: number = 0) {
+//         super(x, y);
+//     }
+//     dist(): number {
+//         let d = super.dist();
+//         return Math.sqrt(d * d + this.z * this.z);
+//     }
+// }
+
+// module Geometry {
+//     export class Square {
+//         constructor(public sideLength: number = 0) {
+//         }
+//         area() {
+//             return Math.pow(this.sideLength, 2);
+//         }
+//     }
+// }
+// let s1 = new Geometry.Square(5);
+// import G = Geometry;
+// let s2 = new G.Square(10);
+// console.log(s1, s2 )
+
+// class Tuple<T1, T2> {
+//     constructor( public item1: T1, public item2: T2) {
+//     }
+// }
+// interface Pair<T> {
+//     item1: T;
+//     item2: T;
+// }
+// let pairToTuple = function <T>(p: Pair<T>) {
+//     return new Tuple(p.item1, p.item2);
+// };
+// let tuple = pairToTuple({ item1: "Hello", item2: "World" });
+// console.log(tuple);
+
+// let name = 'Tyrone';
+// let greeting = `Hi ${name}, how are you?`
+// let multiline = `This is an example of a multiline string`;
+// console.log(name, greeting, multiline);
+
+// interface Person {
+//     readonly name: string;
+//     readonly age: number;
+// }
+// var p1: Person = { name: "Tyrone", age: 42 };
+// p1.age = 25;
+// var p2 = { name: "John", age: 60 };
+// var p3: Person = p2;
+// p3.age = 35;
+// p2.age = 45;
+// console.log(p1, p2, p3);
+
+// class Car {
+//     readonly make: string;
+//     readonly modol: string;
+//     readonly year = 2018;
+//     constructor() {
+//         this.make = "Unknown Make";
+//         this.model = "Unknown Model";
+//     }
+// }
+// console.log(Car);
+
+// let numbers: Array<number> = [0, 1, 2, 3, 4];
+// let moreNumbers: ReadonlyArray<number> = numbers;
+// moreNumbers[5] =5;
+// moreNumbers.push(5);
+// moreNumbers.length = 3;
+// numbers = moreNumbers;
+// console.log(numbers);
+
+// type State = 
+// | { type: "loading" }
+// | { type: "success", value: number }
+// | { type: "error", message: string };
+// declare const state: State;
+// if (state.type === "success") {
+//     console.log(state.value);
+// } else if (state.type === "error") {
+//     console.error(state.message);
+// }
+
+// type OrderSize = "regular" | "large";
+// type OrderItem = "Espresso" | "Cappuccino";
+// type Order = `A ${OrderSize} ${OrderItem}`;
+
+// let order1: Order = "A regular Cappuccino";
+// let order2: Order = "A large Espresso";
+// let order3: Order = "A small Espreeso";
+// console.log(order1);
+// console.log(order2);
+// console.log(order3);
+
+// let arrayOfAnyType = [1, "string", false];
+// for (const val of arrayOfAnyType) {
+//     console.log(val);
+// }
+
+// let list = [4, 5, 6];
+// for (const i of list) {
+//     console.log(i);
+// }
+
+// let list = [4, 5, 6];
+// for (const i in list) {
+//     console.log(i);
+// }
+
+// let foo = {}
+// foo.bar = 123
+// foo.baz = 'hello world'
+// console.log(foo);
+
+// interface Foo {
+//     bar: number;
+//     baz: string;
+// }
+// let foo = {} as Foo;
+// foo.bar = 123; 
+// foo.baz = 'hello world'
+// console.log(foo);
